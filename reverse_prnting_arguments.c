079111b18dc073957403f346a3b8fa0308d7ca6e
Rüveyde Orman <149947158+ruveydeorman@users.noreply.github.com>
2025-10-23 23:08:44 +0300

    Add files via upload

---
+++ reverse_prnting_arguments.c
@@ -0,0 +1,13 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char *argv[])
+{     
+    int i;
+    for(i=argc-1; i>=0; i--){
+        printf("%s\n",argv[i]);
+        
+         
+    }
+    return 0;
+}
